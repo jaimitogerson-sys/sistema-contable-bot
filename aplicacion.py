b1831686d72437ad32df2ca18a91669abe7486d3
jaimitogerson-sys <jaimitogerson@gmail.com>
2026-02-21 01:12:10 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -194,5 +194,7 @@ hilo = threading.Thread(target=revisar_drive)
 hilo.daemon = True
 hilo.start()
 
+# ================== INICIO SERVIDOR ==================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))  # Render asigna PORT automáticamente
+    app.run(host="0.0.0.0", port=port)
